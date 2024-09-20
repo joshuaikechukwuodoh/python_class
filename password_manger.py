45b77fd0590122a896612945b5f2099d307efe56
Odoh Joshua ikechukwu <113645159+joshuaikechukwuodoh@users.noreply.github.com>
2024-09-21 00:01:40 +0100

    Update password_manger.py

--- password_manger.py
+++ password_manger.py
@@ -1,39 +1,122 @@
-password = input("Enter you password: ")
-view = ""
-def view(password):
-    
-    if password == view:
-        view.append(password)
-        print(view)
-    
+
+"""
+start_name = input("Enter your name:")
+
+print(f"Welcome sir/madam {start_name}")
+
+
 
 def add():
+    name = input("Enter your username:\n ")
+    pwd = input("Enter your password: \n")
+    file = open("password.txt" ,"a") 
+    file.write(f"{name}    {pwd} \n")
+    file.close()
+    print("sucessfully created password file:" + file.name)
+
+
+
+def view():
+    file = open("password.txt","a")
+    file.read()
+    file.close()
+    print(file.read())
     
-    pass
+    
+    
+
+
 
 while True:
-    user_input = input("would you like to add or view your new password click(view or add) ").lower()
-    
-    if user_input == "q":
-        
-        print("Quitting...")
-        break
-    elif user_input == "add":
-        
-        print("You selected 'add'.")
-          view()
-        
-    elif user_input == "view":
-        
-        print("You selected 'view'.")
-        
+    user_chioce = input("Enter any of the mode of your password choosen (view/add)")
+    if user_chioce == "add":
         add()
+    elif user_chioce == "view":
+        view()
+    else: 
+     if user_chioce not in ["view", "add"]:
+        print("Invalid choice!")
     
+    continue
+"""
+"""""
+start_name = input("Enter your name: ")
+print(f"Welcome sir/madam {start_name}")
+
+def add():
+    name = input("Enter your username:\n ")
+    pwd = input("Enter your password:\n ")
+    file = open("password.txt", "a") 
+    file.write(name + " " + pwd + "\n")  # Added space and newline for readability
+    file.close()
+    print("Successfully created password file.")
+
+def view():
+    file = open("password.txt", "r")  # Changed mode to 'r' for reading
+    data = file.read()  # Storing the file content
+    file.close()
+    print(data)  # Printing the content
+
+while True:
+    user_choice = input("Enter any mode (view/add): ")
+    if user_choice == "add":
+        add()
+    elif user_choice == "view":
+        view()
     else:
-        print("Invalid input. Please enter 'add', 'view', or 'Q' to quit.")
-        
-user = view(password)    
-print(user)    
+        print("Invalid choice!")
+    
+    continue
+"""
 
 
+from cryptography.fernet import Fernet
 
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''
+
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
+
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:",
+                  fer.decrypt(passw.encode()).decode())
+
+
+def add():
+    name = input('Account Name: ')
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
+
+while True:
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
